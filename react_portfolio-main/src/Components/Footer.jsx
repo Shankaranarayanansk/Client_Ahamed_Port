4529662716c600914967357b20a99d47ad9dc7cf
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2024-02-08 20:06:40 +0530

    Added an responsive for img

--- react_portfolio-main/src/Components/Footer.jsx
+++ react_portfolio-main/src/Components/Footer.jsx
@@ -4,11 +4,19 @@ import { FaLinkedin } from 'react-icons/fa';
 const Footer = () => {
   return (
     <div className="bg-gray-800 text-sm p-4 text-center text-white relative">
-      <div className="absolute top-0 right-0 m-4 flex items-center">
-        <a href="https://www.linkedin.com/in/shankaranarayanansk/" target="_blank" rel="noopener noreferrer" className="hidden md:block">
+      <div className="hidden md:flex absolute top-0 right-0 m-4 items-center">
+        <a href="https://www.linkedin.com/in/shankaranarayanansk/" target="_blank" rel="noopener noreferrer">
           <FaLinkedin className="h-6 w-6 text-blue-500 mr-2" />
         </a>
-        <h4 className="text-white hidden md:block">Shankaranarayanansk <span style={{ color: 'grey' }}>developer</span></h4>
+        <h4 className="text-white">Shankaranarayanansk <span style={{ color: 'grey' }}>developer</span></h4>
+      </div>
+      <div className="md:hidden absolute bottom-0 left-0 right-0 flex justify-center items-center">
+        <a href="https://www.linkedin.com/in/shankaranarayanansk/" target="_blank" rel="noopener noreferrer" className="mr-2">
+          <FaLinkedin className="h-6 w-6 text-blue-500" />
+        </a>
+        <h4 className="text-white">Shankaranarayanansk <span style={{ color: 'grey' }}>developer</span></h4>
+      </div>
+      <div className="md:hidden pt-10">
       </div>
       Copyright © 2024 ahamedthameem. All Rights reserved.
     </div>
--- react_portfolio-main/src/Components/Hero.jsx
+++ react_portfolio-main/src/Components/Hero.jsx
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "../styles.css";
-const technologies = ["Aptitude", "Verbal Reasoning", "Soft Skills", "Life Skills", "Business Communication", "Finance", "Marketing", "Insurance", "Banking", "Mechanical Tech"];
+
+const technologies = [
+  "Aptitude",
+  "Verbal Reasoning",
+  "Soft Skills",
+  "Life Skills",
+  "Business Communication",
+  "Finance",
+  "Marketing",
+  "Insurance",
+  "Banking",
+  "Mechanical Tech",
+];
 
 function Hero() {
   const [techIndex, setTechIndex] = useState(0);
-  const [setLetterIndex] = useState(0);
   const [displayText, setDisplayText] = useState("");
   const [isAdding, setIsAdding] = useState(true);
   const [isWaiting, setIsWaiting] = useState(false);
+  const [isMobileView, setIsMobileView] = useState(false);
 
   const scrollToContact = () => {
     const contactSection = document.getElementById("contact");
@@ -27,7 +39,6 @@ function Hero() {
             setIsAdding(false);
             setIsWaiting(true);
             setTimeout(() => {
-              setLetterIndex(displayText.length - 1);
               setIsWaiting(false);
             }, 1000);
           }
@@ -44,6 +55,17 @@ function Hero() {
     return () => clearInterval(interval);
   }, [displayText, isAdding, isWaiting, techIndex]);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 768); // Adjust threshold as needed
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Initial check
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const social = [
     {
       logo: "logo-instagram",
@@ -53,7 +75,11 @@ function Hero() {
       logo: "logo-linkedin",
       link: "https://www.linkedin.com/in/thameem-ahamed-ahamed-h-658727240/",
     },
-    { logo: "logo-twitter", link: "https://twitter.com/i/flow/login?redirect_after_login=%2Fthameemahamed96" },
+    {
+      logo: "logo-twitter",
+      link:
+        "https://twitter.com/i/flow/login?redirect_after_login=%2Fthameemahamed96",
+    },
   ];
 
   return (
@@ -62,7 +88,19 @@ function Hero() {
       className="min-h-screen flex py-10 md:flex-row flex-col items-center"
     >
       <div className="flex-1 flex items-center justify-center h-full">
-        <img src="/src\assets\images\Ahamed.png" alt="" class="md:w-11/12 h-full object-cover"/>
+        {isMobileView ? (
+          <img
+            src="/src/assets/images/Ahamed2.png"
+            alt=""
+            className="md:w-11/12 h-full object-cover"
+          />
+        ) : (
+          <img
+            src="/src/assets/images/Ahamed.png"
+            alt=""
+            className="md:w-11/12 h-full object-cover"
+          />
+        )}
       </div>
       <div className="flex-1">
         <div className="md:text-left text-center">
@@ -72,7 +110,7 @@ function Hero() {
               <br />
             </span>
             <div className="flex">
-              <span className="text-gray-400">My Name is{" "}</span>
+              <span className="text-gray-400">My Name is </span>
               <div className="dance-animation ">
                 <h1 className="pl-3 dance-letter-1">A</h1>
                 <h1 className="dance-letter-2">h</h1>
